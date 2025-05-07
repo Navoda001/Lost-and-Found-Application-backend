c44a44220d2ca515591bfe6ee8786459d13b481d
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-05-07 19:31:39 +0530

    first commit

---
+++ src/main/java/com/TrackMyItem/TrackMyItemApplication.java
@@ -0,0 +1,13 @@
+package com.TrackMyItem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackMyItemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackMyItemApplication.class, args);
+	}
+
+}
