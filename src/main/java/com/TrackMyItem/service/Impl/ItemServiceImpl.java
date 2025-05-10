1f323091b9d87486059745dd9c943692fc86ecc1
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-05-10 15:20:41 +0530

    update ItemServiceImpl

--- src/main/java/com/TrackMyItem/service/Impl/ItemServiceImpl.java
+++ src/main/java/com/TrackMyItem/service/Impl/ItemServiceImpl.java
@@ -1,33 +1,75 @@
 package com.TrackMyItem.service.Impl;
 
+import com.TrackMyItem.dao.ItemDao;
 import com.TrackMyItem.dto.ItemDto;
+import com.TrackMyItem.dto.ItemStatuses;
+import com.TrackMyItem.entity.ItemEntity;
+import com.TrackMyItem.exception.ItemNotFoundException;
 import com.TrackMyItem.service.ItemService;
+import com.TrackMyItem.util.EntityDtoConverter;
+import com.TrackMyItem.util.UtilData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
+@RequiredArgsConstructor
+@Transactional
 public class ItemServiceImpl implements ItemService {
+
+    private final ItemDao itemDao;
+    private final EntityDtoConverter entityDtoConverter;
+
     @Override
     public void addItem(ItemDto itemDto) {
+    itemDto.setItemId(UtilData.generateItemId());
+    itemDto.setReportedDate(UtilData.generateTodayDate());
+
+    itemDao.save(entityDtoConverter.convertItemDtoToItemEntity(itemDto));
 
     }
 
     @Override
     public void updateItem(String itemId, ItemDto itemDto) {
 
+        Optional<ItemEntity> foundItem = itemDao.findById(itemId);
+        if(!foundItem.isPresent()) {
+            throw new ItemNotFoundException("Item Not Found");
+        }
+
+        if(itemDto.getItemStatus()==ItemStatuses.FOUND){
+            foundItem.get().setFoundDate(UtilData.generateTodayDate());
+            foundItem.get().setItemStatus(ItemStatuses.FOUND);
+        }else{
+            foundItem.get().setClaimedDate(UtilData.generateTodayDate());
+            foundItem.get().setItemStatus(ItemStatuses.CLAIMED);
+        }
+
     }
 
     @Override
     public void deleteItem(String itemId) {
-
+        Optional<ItemEntity> foundItem = itemDao.findById(itemId);
+        if(!foundItem.isPresent()) {
+            throw new ItemNotFoundException("Item Not Found");
+        }
+        itemDao.delete(foundItem.get());
     }
 
     @Override
     public ItemDto getItemById(String itemId) {
-        return null;
+        if(! itemDao.findById(itemId).isPresent()) {
+            throw new ItemNotFoundException("Item Not Found");
+        }
+        return entityDtoConverter.convertItemEntityToItemDto(itemDao.getReferenceById(itemId));
+
     }
 
     @Override
     public List<ItemDto> getAllItems() {
-        return List.of();
+        return entityDtoConverter.toItemDtoList(itemDao.findAll());
     }
 }
