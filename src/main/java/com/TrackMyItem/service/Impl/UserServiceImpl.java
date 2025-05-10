1d016225deee3bbe85977988c2a3779782025465
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-05-10 16:13:19 +0530

    update UserService and UserServiceImpl

--- src/main/java/com/TrackMyItem/service/Impl/UserServiceImpl.java
+++ src/main/java/com/TrackMyItem/service/Impl/UserServiceImpl.java
@@ -1,26 +1,72 @@
 package com.TrackMyItem.service.Impl;
 
+import com.TrackMyItem.dao.UserDao;
+import com.TrackMyItem.dto.UserAllDto;
 import com.TrackMyItem.dto.UserDto;
+import com.TrackMyItem.entity.ItemEntity;
+import com.TrackMyItem.entity.UserEntity;
+import com.TrackMyItem.exception.ItemNotFoundException;
+import com.TrackMyItem.exception.UserNotFoundException;
 import com.TrackMyItem.service.UserService;
+import com.TrackMyItem.util.EntityDtoConverter;
+import com.TrackMyItem.util.UtilData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
+    private final UserDao userDao;
+    private final EntityDtoConverter entityDtoConverter;
+
     @Override
     public void addUser(UserDto userDto) {
+        userDto.setUserId(UtilData.generateUserId());
+        userDto.setLastLogin(UtilData.generateTodayDate());
+        userDto.setUpdatedAt(UtilData.generateTodayDate());
 
+        userDao.save(entityDtoConverter.convertUserDtoToUserEntity(userDto));
     }
 
     @Override
     public void updateUser(String userId, UserDto userDto) {
+        Optional<UserEntity> foundUser = userDao.findById(userId);
+        if(!foundUser.isPresent()) {
+            throw new UserNotFoundException("User Not Found");
+        }
+        foundUser.get().setFirstName(userDto.getFirstName());
+        foundUser.get().setLastName(userDto.getLastName());
+        foundUser.get().setEmail(userDto.getEmail());
+        foundUser.get().setPhone(userDto.getPhone());
 
+        foundUser.get().setUpdatedAt(UtilData.generateTodayDate());
     }
 
     @Override
     public void deleteUser(String userId) {
-
+        Optional<UserEntity> foundUser = userDao.findById(userId);
+        if(!foundUser.isPresent()) {
+            throw new UserNotFoundException("User Not Found");
+        }
+        userDao.delete(foundUser.get());
     }
 
     @Override
     public UserDto getUserById(String userId) {
-        return null;
+        Optional<UserEntity> foundUser = userDao.findById(userId);
+        if(!foundUser.isPresent()) {
+            throw new UserNotFoundException("User Not Found");
+        }
+        return entityDtoConverter.convertUserEntityToUserDto(foundUser.get());
+    }
+
+    @Override
+    public List<UserAllDto> getAllUsers() {
+        return  entityDtoConverter.toUserAllDtoList(userDao.findAll());
     }
 }
--- src/main/java/com/TrackMyItem/service/UserService.java
+++ src/main/java/com/TrackMyItem/service/UserService.java
@@ -1,10 +1,14 @@
 package com.TrackMyItem.service;
 
+import com.TrackMyItem.dto.UserAllDto;
 import com.TrackMyItem.dto.UserDto;
+
+import java.util.List;
 
 public interface UserService {
     void addUser(UserDto userDto);
     void updateUser(String userId,UserDto userDto);
     void deleteUser(String userId);
     UserDto getUserById(String userId);
+    List<UserAllDto> getAllUsers();
 }
