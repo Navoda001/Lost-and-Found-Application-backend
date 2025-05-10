c5050e0b34cef0bbc6d3d84c7b393ede47e07ab5
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-05-10 16:12:53 +0530

    create UserAllDto

---
+++ src/main/java/com/TrackMyItem/dto/UserAllDto.java
@@ -0,0 +1,11 @@
+package com.TrackMyItem.dto;
+
+import java.io.Serializable;
+
+public class UserAllDto implements Serializable {
+
+    private String userId;
+    private String firstName;
+    private String lastName;
+
+}
