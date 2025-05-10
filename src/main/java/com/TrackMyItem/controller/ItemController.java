662283af41dd02a60ea8b14ac71dc28bc4f1f5c0
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-05-10 17:50:20 +0530

    create ItemController

---
+++ src/main/java/com/TrackMyItem/controller/ItemController.java
@@ -0,0 +1,89 @@
+package com.TrackMyItem.controller;
+
+import com.TrackMyItem.dto.ItemDto;
+import com.TrackMyItem.exception.ItemNotFoundException;
+import com.TrackMyItem.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.cache.spi.support.AbstractReadWriteAccess;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.awt.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/items")
+@RequiredArgsConstructor
+public class ItemController {
+    private final ItemService itemService;
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> addItem(@RequestBody ItemDto itemDto) {
+        if(itemDto== null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        itemService.addItem(itemDto);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @DeleteMapping
+    public ResponseEntity<Void> deleteItem(@RequestParam("itemId") String itemId) {
+        if(itemId == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        try{
+            itemService.deleteItem(itemId);
+            return ResponseEntity.noContent().build();
+        }catch (ItemNotFoundException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @PatchMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateItem(@RequestParam("itemId") String itemId ,@RequestBody ItemDto itemDto) {
+        if(itemDto==null || itemId == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        try {
+            itemService.updateItem(itemId, itemDto);
+            return ResponseEntity.noContent().build();
+        }catch (ItemNotFoundException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<ItemDto> getItemById(@RequestParam("itemId") String itemId) {
+        if(itemId == null){
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+        try {
+            ItemDto itemDto = itemService.getItemById(itemId);
+            return new ResponseEntity<>(itemDto, HttpStatus.OK);
+        }
+        catch (ItemNotFoundException e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @GetMapping("getAllItems")
+    public ResponseEntity<List<ItemDto>> getAllItems() {
+        return new ResponseEntity<>(itemService.getAllItems(), HttpStatus.OK);
+    }
+}
+
+
