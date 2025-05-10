3c5973a6889d7d517d94259246d61b1da1ea2e90
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-05-10 19:19:37 +0530

    fix error with annotations

--- src/main/java/com/TrackMyItem/dto/UserAllDto.java
+++ src/main/java/com/TrackMyItem/dto/UserAllDto.java
@@ -1,7 +1,14 @@
 package com.TrackMyItem.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.io.Serializable;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class UserAllDto implements Serializable {
 
     private String userId;
