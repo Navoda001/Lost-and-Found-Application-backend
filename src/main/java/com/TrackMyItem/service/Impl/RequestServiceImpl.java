e5007b1b9f1f6a8908c0cf5cfcc916ba90d725c6
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-05-10 15:51:17 +0530

    update RequestServiceImpl

--- src/main/java/com/TrackMyItem/service/Impl/RequestServiceImpl.java
+++ src/main/java/com/TrackMyItem/service/Impl/RequestServiceImpl.java
@@ -1,33 +1,65 @@
 package com.TrackMyItem.service.Impl;
 
+import com.TrackMyItem.dao.RequestDao;
 import com.TrackMyItem.dto.RequestDto;
+import com.TrackMyItem.dto.RequestStatuses;
+import com.TrackMyItem.entity.RequestEntity;
+import com.TrackMyItem.exception.RequestNotFoundException;
 import com.TrackMyItem.service.RequestService;
+import com.TrackMyItem.util.EntityDtoConverter;
+import com.TrackMyItem.util.UtilData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class RequestServiceImpl implements RequestService {
+    private final RequestDao requestDao;
+    private final EntityDtoConverter entityDtoConverter;
+
     @Override
     public void addRequest(RequestDto requestDto) {
+        requestDto.setRequestId(UtilData.generateRequestId());
+        requestDto.setRequestDate(UtilData.generateTodayDate());
+        requestDto.setRequestStatus(RequestStatuses.PENDING);
+        requestDao.save(entityDtoConverter.convertRequestDtoToRequestEntity(requestDto));
 
     }
 
     @Override
     public void updateRequest(String requestId, RequestDto requestDto) {
-
+        Optional<RequestEntity> foundRequest = requestDao.findById(requestId);
+        if(!foundRequest.isPresent()) {
+            throw new RequestNotFoundException("Request Not Found");
+        }
+        foundRequest.get().setDecisionDate(UtilData.generateTodayDate());
     }
 
     @Override
     public void deleteRequest(String requestId) {
-
+        Optional<RequestEntity> foundRequest = requestDao.findById(requestId);
+        if(!foundRequest.isPresent()) {
+            throw new RequestNotFoundException("Request Not Found");
+        }
+        requestDao.delete(foundRequest.get());
     }
 
     @Override
     public RequestDto getRequestById(String requestId) {
-        return null;
+        Optional<RequestEntity> foundRequest = requestDao.findById(requestId);
+        if(!foundRequest.isPresent()) {
+            throw new RequestNotFoundException("Request Not Found");
+        }
+        return entityDtoConverter.convertRequestEntityToRequestDto(foundRequest.get());
     }
 
     @Override
     public List<RequestDto> getAllRequests() {
-        return List.of();
+        return entityDtoConverter.toRequestDtoList(requestDao.findAll());
     }
 }
