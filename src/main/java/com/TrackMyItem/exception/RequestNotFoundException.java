9e2dc91d74cb39553c3adde2ba934d933460c73e
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-05-10 12:08:52 +0530

    create not found exceptions

---
+++ src/main/java/com/TrackMyItem/exception/RequestNotFoundException.java
@@ -0,0 +1,13 @@
+package com.TrackMyItem.exception;
+
+public class RequestNotFoundException extends RuntimeException {
+    public RequestNotFoundException() {
+
+    }
+    public RequestNotFoundException(String message) {
+
+    }
+    public RequestNotFoundException(String message, Throwable cause) {
+
+    }
+}
---
+++ src/main/java/com/TrackMyItem/exception/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package com.TrackMyItem.exception;
+
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException() {
+
+    }
+    public UserNotFoundException(String message) {
+
+    }
+    public UserNotFoundException(String message, Throwable cause) {
+
+    }
+}
