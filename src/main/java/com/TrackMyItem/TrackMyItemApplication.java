fb6b0ea08dba837f085b2677748142fcbf566b55
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-05-10 12:04:57 +0530

    add ModelMapper

--- src/main/java/com/TrackMyItem/TrackMyItemApplication.java
+++ src/main/java/com/TrackMyItem/TrackMyItemApplication.java
@@ -1,12 +1,23 @@
 package com.TrackMyItem;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
+@EnableJpaRepositories
+@EnableWebMvc
 public class TrackMyItemApplication {
+	@Bean
+	public ModelMapper modelMapper() {
+		return new ModelMapper();
+	}
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(TrackMyItemApplication.class, args);
 	}
 
