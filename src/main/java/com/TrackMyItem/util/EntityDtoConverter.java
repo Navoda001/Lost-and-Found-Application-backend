ec63aadfbc21488c769d3586bf30dc17fa23946a
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-05-10 12:16:23 +0530

    create EntityDtoConverter

---
+++ src/main/java/com/TrackMyItem/util/EntityDtoConverter.java
@@ -0,0 +1,54 @@
+package com.TrackMyItem.util;
+
+import com.TrackMyItem.dto.ItemDto;
+import com.TrackMyItem.dto.RequestDto;
+import com.TrackMyItem.dto.UserDto;
+import com.TrackMyItem.entity.ItemEntity;
+import com.TrackMyItem.entity.RequestEntity;
+import com.TrackMyItem.entity.UserEntity;
+import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class EntityDtoConverter {
+    private final ModelMapper modelMapper;
+
+    //Item
+    public ItemEntity convertItemDtoToBookEntity(ItemDto itemDto) {
+        return modelMapper.map(itemDto, ItemEntity.class);
+    }
+    public ItemDto convertItemEntityToItemDto(ItemEntity itemEntity) {
+        return modelMapper.map(itemEntity, ItemDto.class);
+    }
+    public List<ItemDto> toItemDtoList(List<ItemEntity> itemEntityList) {
+        return modelMapper.map(itemEntityList, new TypeToken<List<ItemDto>>(){}.getType());
+    }
+
+    //Request
+    public RequestEntity convertRequestDtoToRequestEntity(RequestDto requestDto) {
+        return modelMapper.map(requestDto, RequestEntity.class);
+    }
+    public RequestDto convertRequestEntityToRequestDto(RequestEntity requestEntity) {
+        return modelMapper.map(requestEntity, RequestDto.class);
+    }
+    public List<RequestDto> toRequestDtoList(List<RequestEntity> requestEntityList) {
+        return modelMapper.map(requestEntityList, new TypeToken<List<RequestDto>>(){}.getType());
+    }
+
+    //User
+    public UserEntity convertUserDtoToUserEntity(UserDto userDto) {
+        return modelMapper.map(userDto, UserEntity.class);
+    }
+    public UserDto convertUserEntityToUserDto(UserEntity userEntity) {
+        return modelMapper.map(userEntity, UserDto.class);
+    }
+    public List<UserDto> toUserDtoList(List<UserEntity> userEntityList) {
+        return modelMapper.map(userEntityList, new TypeToken<List<UserDto>>(){}.getType());
+    }
+
+}
