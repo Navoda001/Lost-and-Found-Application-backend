3b1200538c5ee0d6f500a7abb5e512943b6ede22
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-05-12 17:52:57 +0530

    add CORSConfig

---
+++ src/main/java/com/TrackMyItem/config/CORSConfig.java
@@ -0,0 +1,23 @@
+package com.TrackMyItem.config;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CORSConfig {
+    @Bean
+    public WebMvcConfigurer configurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                //add the configs
+                registry
+                        .addMapping("/**")
+                        .allowedOrigins("http://localhost:3000")
+                        .allowedMethods("GET","POST","PATCH","PUT","DELETE","OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
