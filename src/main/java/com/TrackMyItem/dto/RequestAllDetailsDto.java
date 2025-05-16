cb04cf9a2a2f59283e521ecb2d93da69a50ddd3a
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-05-16 23:46:34 +0530

    create RequestAllDetailsDto

---
+++ src/main/java/com/TrackMyItem/dto/RequestAllDetailsDto.java
@@ -0,0 +1,23 @@
+package com.TrackMyItem.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class RequestAllDetailsDto {
+    private String requestId;
+    private String itemId;
+    private String itemName;
+    private String userId;
+    private String userName;
+    private RequestStatuses requestStatus;
+    private String message;
+    private LocalDate requestDate;
+    private LocalDate decisionDate;
+    private String getDecisionBy;
+}
