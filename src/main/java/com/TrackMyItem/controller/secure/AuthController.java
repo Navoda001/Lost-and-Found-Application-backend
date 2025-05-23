30caf572acf30573edeb60c8ad397cdd6aac9846
Navoda Chathurya <159541264+Navoda001@users.noreply.github.com>
2025-05-23 18:02:33 +0530

    create AuthController

---
+++ src/main/java/com/TrackMyItem/controller/secure/AuthController.java
@@ -0,0 +1,29 @@
+package com.TrackMyItem.controller.secure;
+
+import com.TrackMyItem.dto.secure.AllUsersDto;
+import com.TrackMyItem.dto.secure.JWTAuthResponse;
+import com.TrackMyItem.dto.secure.SignIn;
+import com.TrackMyItem.entity.RequestEntity;
+import com.TrackMyItem.service.secure.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthService authService;
+    @PostMapping("signin")
+    public ResponseEntity<JWTAuthResponse> signIn(@RequestBody SignIn signIn){
+        return new ResponseEntity<>(authService.signIn(signIn),HttpStatus.OK);
+    }
+    @PostMapping("signup")
+    public ResponseEntity<JWTAuthResponse> signUp(@RequestBody AllUsersDto signUp){
+        return new ResponseEntity<>(authService.signUp(signUp),HttpStatus.CREATED);
+    }
+}
